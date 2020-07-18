7b21d89eee431360d790085925e4ed7ebcc3ac8a
Nguyen Phat <phatnt.uit@gmail.com>
2020-07-18 19:11:02 +0700

    Update basic package with cleanup

--- packages/basic/gulpfile.js
+++ packages/basic/gulpfile.js
@@ -1,7 +1,16 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const { src, dest, series } = require('gulp');
+const del = require('del');
+
+function clean() {
+  return del([
+    "dist/*"
+  ]);
 }
 
-exports.default = defaultTask;
+function transpile() {
+  return src('src/*.js')
+    .pipe(dest('dist/'));
+}
+
+exports.build = series(clean, transpile);
 // https://gulpjs.com/docs/en/getting-started/javascript-and-gulpfiles#splitting-a-gulpfile
